837b63676c1fc0f91d0bf172d02ab87d82f5d492
jiyong.yoon <jiyong.yoon@wizcore.co.kr>
2025-07-28 16:46:39 +0900

    git init

---
+++ src/main/java/dev/yoon/study_springboot_websocket_stomp/StudySpringbootWebsocketStompApplication.java
@@ -0,0 +1,13 @@
+package dev.yoon.study_springboot_websocket_stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootWebsocketStompApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudySpringbootWebsocketStompApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/yoon/study_springboot_websocket_stomp/StudySpringbootWebsocketStompApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yoon.study_springboot_websocket_stomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootWebsocketStompApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
